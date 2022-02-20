1a0cc4c41922a45ebd288cb8999e1d42416795ed
Matthew Stead <mattstead6@gmail.com>
2022-02-20 18:06:02 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-function addingEventListener() {const input = document.getElementById('input');
 
-function clickAlert() {
-  alert('I was clicked!');
+
+const input = document.getElementById('input');
+
+function addingEventListener() {
+      input.addEventListener('click', ()=>{alert('I was clicked!');});
 }
 
-input.addEventListener('click', clickAlert);
-}
+
+
