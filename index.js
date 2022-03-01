a84f58d8090e0fed1cdac49e276b68d7e08a4954
Matthew Stead <mattstead6@gmail.com>
2022-03-01 10:14:22 -0500

    commit tree c11315f071f7660cf4f7d107d29269af23ab3ab3

--- index.js
+++ index.js
@@ -1,10 +1,54 @@
 
+const input = document.querySelector('#input')
 
-const input = document.getElementById('input');
 
-function addingEventListener() {
-      input.addEventListener('click', ()=>{alert('I was clicked!');});
+
+
+function addingEventListener(){
+      input.addEventListener('click', addingEventListener)
+      alert("I was clicked")
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const input = document.getElementById('input');
+
+// function addingEventListener() {
+//       input.addEventListener('click', ()=>{alert('I was clicked!');});
+// }
 
 
 
